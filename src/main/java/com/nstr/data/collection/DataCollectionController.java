c118277cd49cf06b18b0d69d14135eefde0d98af
zzq <191550636@qq.com>
2018-10-02 20:39:46 +0800

    json和string相关的工具类

--- src/main/java/com/nstr/data/collection/DataCollectionController.java
+++ src/main/java/com/nstr/data/collection/DataCollectionController.java
@@ -14,21 +14,23 @@ import java.io.OutputStream;
 import java.util.Map;
 
 @Controller
-@RequestMapping("/data/collection")
+@RequestMapping("/data/nstr")    //url尽量短，减少没必要的字符
 public class DataCollectionController {
 
     @RequestMapping(value = "/log.gif")
     public void analysis(String args, HttpServletRequest request,
                          HttpServletResponse response) throws IOException {
+		    //打印日志
         System.out.println(args);
-		    //日志收集
+        //获取ip信息
         String ipAddr = IPAddressUtil.getIpAddr(request);
         System.out.println(ipAddr);
-
+        //将其他的参数信息解析为map参数
         Map<String, Object> infos = URLUtil.parseArgs(args);
         System.out.println(infos);
 
         //使用jackson将map转换成bean
+
 
         //返回信息
         response.setHeader("Pragma", "No-cache");
