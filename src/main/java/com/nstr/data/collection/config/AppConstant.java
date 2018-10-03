498cd36fddb1718d30103b20cdf618f2b8deab66
zzq <191550636@qq.com>
2018-10-03 10:54:46 +0800

    增加ip插件策略名称

--- src/main/java/com/nstr/data/collection/config/AppConstant.java
+++ src/main/java/com/nstr/data/collection/config/AppConstant.java
@@ -8,7 +8,10 @@ public class AppConstant {
   public static final String IPNET_RESOURCE="/ip/ipipnet/17monipdb.dat";
   public static final String GEOIP2_CITY_RESOURCE="/ip/GeoLite2-City.mmdb";
 
-  public static List<String> ipPluginNames = Arrays.asList("geoip", "ipipnet");
+  /**
+   * 启用的ip解析的插件，分别是geoip（默认），ipipnet，不启用解析，不启用解析并且对ip进行模糊处理
+   */
+  public static List<String> ipPluginNames = Arrays.asList("geoip", "ipipnet", "NO", "FUZZY");
   public static final String UNKOWN_INFO = "UNKOWN";
 
 }
