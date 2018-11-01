0eeaa52e5afc32b5bc1ffd00c5457da8845d8330
zzq <191550636@qq.com>
2018-11-01 21:08:49 +0800

    按照周期备份和按照月份备份两种方法

--- src/main/java/com/nstr/data/collection/schedule/HistoryCommentTask.java
+++ src/main/java/com/nstr/data/collection/schedule/HistoryCommentTask.java
@@ -1,14 +1,28 @@
 package com.nstr.data.collection.schedule;
 
+import com.nstr.data.collection.model.pojo.HistoryComment;
+import com.nstr.data.collection.service.BackupCommentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
 
 @Component
 public class HistoryCommentTask {
 
-    @Scheduled(cron = "0/5 * *  * * ? ")   //每5秒执行一次
+    private static final Logger logger = LoggerFactory.getLogger(HistoryCommentTask.class);
+
+    private static final String SCHEDULE_CRON = "0/5 * *  * * ? ";//每5秒执行一次
+
+    @Resource
+    private BackupCommentService backupCommentService;
+
+    @Scheduled(cron = SCHEDULE_CRON)
     public void backupHistoryCommentData(){//将历史数据进行备份到新表，并删除原来的
-        System.out.println("进入测试");
+        logger.info("定时备份整理评论数据:"+SCHEDULE_CRON);
+        backupCommentService.backup();
     }
 
 }
--- src/main/java/com/nstr/data/collection/service/impl/BackupCommentServiceImpl.java
+++ src/main/java/com/nstr/data/collection/service/impl/BackupCommentServiceImpl.java
@@ -2,7 +2,9 @@ package com.nstr.data.collection.service.impl;
 
 import com.nstr.data.collection.config.AppConstant;
 import com.nstr.data.collection.repository.CommonMapper;
+import com.nstr.data.collection.repository.ResourceCommentMapper;
 import com.nstr.data.collection.service.BackupCommentService;
+import com.nstr.data.collection.service.ResourceCommentService;
 import com.nstr.data.collection.util.DateUtil;
 import com.nstr.data.collection.util.TableUtil;
 import org.slf4j.Logger;
@@ -10,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -20,20 +23,36 @@ public class BackupCommentServiceImpl implements BackupCommentService {
 
     @Resource
     private CommonMapper commonMapper;
+    @Resource
+    private ResourceCommentService resourceCommentService;
 
 
     @Override
     public void backup() {
-        //获取上月信息或者上周信息（月备份和周备份）
-        String dateStr = TableUtil.generateDateStr(AppConstant.BACKUP_TYPE);
 
-        //一般都是这个月去生成上个月的备份表，或者今天生成昨天的备份表
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH) + 1;
+        int day = calendar.get(Calendar.DATE);
+        if("month".equals(AppConstant.BACKUP_TYPE)){
+            calendar.add(Calendar.MONTH, -1);
+            year = calendar.get(Calendar.YEAR);
+            month = calendar.get(Calendar.MONTH) +1;
+        }else if("week".equals(AppConstant.BACKUP_TYPE)){
+            calendar.add(Calendar.DATE, -7);
+            year = calendar.get(Calendar.YEAR);
+            month = calendar.get(Calendar.MONTH) +1;
+            day = calendar.get(Calendar.DATE);
+        }
+
+        //一般都是这个月去生成上个月的备份表，或者今天生成昨天的备份表的名称
         String table = TableUtil.generateTableName(AppConstant.COMMENT_TABLE,AppConstant.BACKUP_TYPE);
         List<String> exists = commonMapper.exists(table);
         if(exists == null || exists.size() == 0){
             commonMapper.createBackUpTable(table);
-            Long[] bes = DateUtil.getLongTypeBeginAndEnd("month",1,2,0);
+            Long[] bes = DateUtil.getLongTypeBeginAndEnd(AppConstant.BACKUP_TYPE,year,month,day);
             commonMapper.transferData(bes[0], bes[1], new Date(), table);
+//            resourceCommentService.delete(bes[0], bes[1]);
         }else{
             logger.warn("数据库已经存在备份数据表："+table);
         }
